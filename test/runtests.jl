525845c53454c55ebe0b9555e70e4c856a75a6fe
tiagoseq <73851660+tiagoseq@users.noreply.github.com>
2021-06-01 09:37:10 +0100

    Added convergence tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,13 @@
 using Test, SNFE
 
-@testset "prob" begin
+#@testset "prob" begin
     
-end
+#end
 
-@testset "solve" begin
-    tol = 1e-3
-
+@testset "solve_1D" begin
     # Test the convergence using an analytical bump solution
     # Laing, Troy, Gutkin, and Ermentrou - Multiple bumps in a neuronal model of working memory
+    tol = 0.1
     function W(x)
         if x != 0
             return (((1-exp(-1.8*abs(x)))*(3.5/1.8) + (-1+exp(-1.52*abs(x)))*(3/1.52))*x)/abs(x) 
@@ -23,12 +22,67 @@ end
     S(V) = convert(Float64,V>0.0) # Heavyside function H(V)
     α  = 1.0
     v  = 900000.0
-    V0 = sole
+    V0 = exact_solution
     L  = 20
     T  = 20.0
     n  = 200
+    tj = [0.2,1.0,5.0,10.0,20.0]
 
-    input = Input1D(α,v,V0,L,N,T,n,I,K,S);
-    prob  = probSNFE(input)
-    V     = solveSNFE(prob,[0.2,1.0,5.0,10.0,20.0])
+    in02   = Input1D(α,v,V0,L,100,T,n,I,K,S);
+    prob02 = probSNFE(in02)
+    in01   = Input1D(α,v,V0,L,200,T,n,I,K,S);
+    prob01 = probSNFE(in01)
+    in005  = Input1D(α,v,V0,L,400,T,n,I,K,S);
+    prob005= probSNFE(in005)
+
+    V02  = solveSNFE(prob02,tj)
+    V01  = solveSNFE(prob01,tj)
+    V005 = solveSNFE(prob005,tj)
+
+    e_x = zeros(3,length(tj))
+    for i = 1:length(tj)
+        e_x[1,i] = abs(V02(i)[51]-exact_solution(0))
+        e_x[2,i] = abs(V01(i)[101]-exact_solution(0))
+        e_x[3,i] = abs(V005(i)[201]-exact_solution(0))
+        @test isapprox(2.0,e_x[1,i]./e_x[2,i],atol = tol)
+        @test isapprox(2.0,e_x[2,i]./e_x[3,i],atol = tol)
+    end
+end
+
+@testset "solve_2D" begin
+    using SpecialFunctions
+    tol = 0.1
+    function I(x,y,t)
+        λ = 8.0
+        b = pi/λ*(erf(sqrt(λ)))^2 #-1 to 1
+        return -tanh(exp(-t))*b
+    end
+    function K(x,y)
+        λ = 8.0
+        return exp(-λ*(x^2+y^2))
+    end
+    S(V) = tanh(V)
+    
+    α  = 1.0
+    v  = 900000.0
+    V0 = 1.0
+    L  = 2
+    N  = 200
+    T  = 0.1
+    tj = [0.02,0.04,0.06,0.08,0.1]
+    
+    in002   = Input2D(α,v,V0,L,N,T,5,I,K,S);
+    in001   = Input2D(α,v,V0,L,N,T,10,I,K,S);
+    prob002 = probSNFE(in002)
+    prob001 = probSNFE(in001)
+
+    V002 = solveSNFE(prob002,tj)
+    V001 = solveSNFE(prob001,tj)
+
+    e_t = zeros(2,length(tj))
+    for i = 1:length(tj)
+        e_t[1,i] = abs(V002(i)[101]-exp(-tj[i]))
+        e_t[2,i] = abs(V001(i)[101]-exp(-tj[i]))
+        @test isapprox(2.0,e_t[1,i]./e_t[2,i],atol = tol)
+    end    
 end
